6d08718d6130218f4f556984ef04a2558f338eaf
Jalia Evans <jalia@nete.com>
2019-06-18 14:53:57 -0400

    working on year, degree and rank filters

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
-    $(document).ready(function () {
-        var data = null,
-            genderMatch = [],
-            degreeMatch = [],
-            raceMatch = [],
-            filterMale,
-            filterFemale,
-            shapeFill,
-            shape,
-            rank;
+$(document).ready(function () {
+    var data = null,
+        genderMatch = [],
+        degreeMatch = [],
+        raceMatch = [],
+        filterMale,
+        filterFemale,
+        filterDegree,
+        filterYear,
+        year,
+        faculty,
+        shapeFill,
+        shape,
+        rank;
 
-        async function getData () {
-            data = await d3.json("data.json",);
-        };
+    async function getData() {
+        data = await d3.json("data.json");
+    };
 
-        function degreeCheck (degree) {
-            degreeMatch = [];
+    function degreeCheck(degree, filter) {
+        degreeMatch = [];
+
+        if (filter) {
+            if (degree.hasOwnProperty(filter)) {
+                if (filter === 'MD') {
+                    degree.MD.forEach(function (person) {
+                    degreeMatch.push(person);      
+                })} else if (filter === 'mdPHD') {
+                    degree.mdPHD.forEach(function (person) {
+                    degreeMatch.push(person);
+                })} else if (filter === 'PHD') {
+                    degree.PHD.forEach(function (person) {
+                        degreeMatch.push(person);
+                })}
+            };
+        } else {
+            // no degree filter selected
             if (degree.hasOwnProperty('MD')) {
                 // no gender selected
-                gender = degree.MD.forEach(function(person) {
-                    if(!filterMale && !filterFemale) {
+                gender = degree.MD.forEach(function (person) {
+                    if (!filterMale && !filterFemale) {
                         degreeMatch.push(person);
                     }
                     // female selected
                     else if (filterFemale) {
-                            if (person.female) {
-                                degreeMatch.push(person);
-                            }
+                        if (person.female) {
+                            degreeMatch.push(person);
                         }
+                    }
                     // male selected
                     else if (filterMale) {
                         if (person.male) {
@@ -35,8 +55,8 @@
                     }
                 })
             } else if (degree.hasOwnProperty('mdPHD')) {
-                gender = degree.mdPHD.forEach(function(person) {
-                    if(!filterMale && !filterFemale) {
+                gender = degree.mdPHD.forEach(function (person) {
+                    if (!filterMale && !filterFemale) {
                         degreeMatch.push(person);
                     }
                     // female selected
@@ -53,8 +73,8 @@
                     }
                 })
             } else if (degree.hasOwnProperty('PHD')) {
-                gender = degree.PHD.forEach(function(person) {
-                    if(!filterMale && !filterFemale) {
+                gender = degree.PHD.forEach(function (person) {
+                    if (!filterMale && !filterFemale) {
                         degreeMatch.push(person);
                     }
                     // female selected
@@ -70,240 +90,203 @@
                         }
                     }
                 })
-        }
-        }
-
-        function genderCheck (degree) {
-            genderMatch = [];
-            if (degree.hasOwnProperty('MD')) {
-                // no gender selected
-                gender = degree.MD.forEach(function(person) {
-                    if(!filterMale && !filterFemale) {
-                        genderMatch.push(person);
-                    }
-                    // female selected
-                    else if (filterFemale) {
-                        if (person.female) {
-                            genderMatch.push(person);
-                        }
-                    // male selected
-                    } else if (filterMale) {
-                        if (person.male) {
-                            genderMatch.push(person);
-                        }
-                    }
-                })
-            } else if (degree.hasOwnProperty('mdPHD')) {
-                gender = degree.mdPHD.forEach(function(person) {
-                    if(!filterMale && !filterFemale) {
-                        genderMatch.push(person);
-                    }
-                    // female selected
-                    else if (filterFemale) {
-                            if (person.female) {
-                                genderMatch.push(person);
-                            }
-                        }
-                    // male selected
-                    else if (filterMale) {
-                        if (person.male) {
-                            genderMatch.push(person);
-                        }
-                    }
-                })
-            } else if (degree.hasOwnProperty('PHD')) {
-                gender = degree.PHD.forEach(function(person) {
-                    if(!filterMale && !filterFemale) {
-                        genderMatch.push(person);
-                    }
-                    // female selected
-                    else if (filterFemale) {
-                            if (person.female) {
-                                genderMatch.push(person);
-                            }
-                        }
-                    // male selected
-                    else if (filterMale) {
-                        if (person.male) {
-                            genderMatch.push(person);
-                        }
-                    }
-                })
-        }}
-        
-
-        function colorPicker (person) {
-            if (person.race === 'asian') {
-                shapeFill = '#f4831f ' + person.race;
-            } else if (person.race === 'black') {
-                shapeFill = '#f5cb05 ' + person.race;
-            } else if (person.race === 'islander') {
-                shapeFill = '#0f5689 ' + person.race;
-            } else if (person.race === 'white') {
-                shapeFill = '#df4a50 ' + person.race;
-            } else if (person.race === 'hispanic') {
-                shapeFill = '#b5d25d ' + person.race;
-            } else if (person.race === 'native') {
-                shapeFill = '#2f89cf ' + person.race;
-            }
-            return shapeFill;
-        }
-
-
-        function shapePicker (degree) {
-            if (degree.hasOwnProperty('mdPHD')) {
-                console.log(degree.mdPHD);
             }
         }
 
-        getData().then(function () {
-            // get list of years
-            var academicYears = [],
-                facultyRank,
-                facultyRace,
-                facultyFemale,
-                facultyMale,
-                facultyTotal;
 
-            data.forEach(function(item) {
-                academicYears.push(item.academicYear);
+    }
 
-                var faculty = item.faculty;
-                
-                // get shapes for each position
-                faculty.forEach(function (job) {
-                    if (job.chair) {
-                        rank = job.chair;
-                        shape = 'circle';
-
-                        rank.forEach(function(degree) {
-                            degreeCheck(degree);
-                            
-                            // checking only mdPHD - filtering
-                            if (degree.hasOwnProperty('mdPHD')) {
-                                // console.log(degreeMatch);
-                            };
-                            
-                            // filtering females only
-                            filterFemale = true;
-                            genderCheck(degree);
-                            console.log(genderMatch);
-
-                            // filtering fills for shape based on degree filter
-                            degreeMatch.forEach(function (person) {
-                                colorPicker(person);
-                            });
-
-                            // filtering fills for gender filter
-                            genderMatch.forEach(function (person) {
-                                colorPicker(person);
-                                console.log(shapeFill);
-                            });
-                        })
-
-                    } else if (job.professor) {
-                        rank = job.professor;
-                        shape = 'diamond';
-                    };  
-                })
-            });
-
-            // degree and gender checks for chairs
-            data[0].faculty[0].chair.forEach(function(person) {
-                degreeCheck(person);
-                if (degreeMatch.length > 0) {
-                    // console.log(degreeMatch);
-                }
-
-                // gender check
-                genderCheck(person);
-                if (genderMatch.length > 0) {
-                    // console.log(genderMatch);
-                }
-
-                // raceCheck(person);
-            });
-
-            // degree and gender checks for professors
-            // data[0].faculty[1].professor.forEach(function(person) {
-            //     degreeCheck(person);
-            //     if (degreeMatch.length > 0) {
-            //         console.log(degreeMatch);
-            //     }
-
-            //     // gender check
-            //     genderCheck(person);
-            //     if (genderMatch.length > 0) {
-            //         console.log(genderMatch);
-            //     }
-            // });
-
-            }
-        )
-    });
-
-    /*
-    degreeMatch = [];
+    function genderCheck(degree) {
+        genderMatch = [];
         if (degree.hasOwnProperty('MD')) {
             // no gender selected
-            gender = degree.MD.forEach(function(person) {
-                if(!filterMale && !filterFemale) {
-                    console.log('no filters');
-                    degreeMatch.push(person);
+            gender = degree.MD.forEach(function (person) {
+                if (!filterMale && !filterFemale) {
+                    genderMatch.push(person);
                 }
                 // female selected
                 else if (filterFemale) {
-                        if (person.female) {
-                            console.log('female filters');
-                            degreeMatch.push(person);
-                        }
+                    if (person.female) {
+                        genderMatch.push(person);
                     }
-                // male selected
-                else if (filterMale) {
+                    // male selected
+                } else if (filterMale) {
                     if (person.male) {
-                        degreeMatch.push(person);
+                        genderMatch.push(person);
                     }
                 }
             })
         } else if (degree.hasOwnProperty('mdPHD')) {
-            gender = degree.MD.forEach(function(person) {
-                if(!filterMale && !filterFemale) {
-                    console.log('no filters');
-                    degreeMatch.push(person);
+            gender = degree.mdPHD.forEach(function (person) {
+                if (!filterMale && !filterFemale) {
+                    genderMatch.push(person);
                 }
                 // female selected
                 else if (filterFemale) {
-                        if (person.female) {
-                            console.log('female filters');
-                            degreeMatch.push(person);
-                        }
+                    if (person.female) {
+                        genderMatch.push(person);
                     }
+                }
                 // male selected
                 else if (filterMale) {
                     if (person.male) {
-                        degreeMatch.push(person);
+                        genderMatch.push(person);
                     }
                 }
             })
         } else if (degree.hasOwnProperty('PHD')) {
-            gender = degree.MD.forEach(function(person) {
-                if(!filterMale && !filterFemale) {
-                    console.log('no filters');
-                    degreeMatch.push(person);
+            gender = degree.PHD.forEach(function (person) {
+                if (!filterMale && !filterFemale) {
+                    genderMatch.push(person);
                 }
                 // female selected
                 else if (filterFemale) {
-                        if (person.female) {
-                            console.log('female filters');
-                            degreeMatch.push(person);
-                        }
+                    if (person.female) {
+                        genderMatch.push(person);
                     }
+                }
                 // male selected
                 else if (filterMale) {
                     if (person.male) {
-                        degreeMatch.push(person);
+                        genderMatch.push(person);
                     }
                 }
             })
         }
-        */
+    }
+
+
+    function colorPicker(person) {
+        if (person.race === 'asian') {
+            shapeFill = '#f4831f ' + person.race;
+        } else if (person.race === 'black') {
+            shapeFill = '#f5cb05 ' + person.race;
+        } else if (person.race === 'islander') {
+            shapeFill = '#0f5689 ' + person.race;
+        } else if (person.race === 'white') {
+            shapeFill = '#df4a50 ' + person.race;
+        } else if (person.race === 'hispanic') {
+            shapeFill = '#b5d25d ' + person.race;
+        } else if (person.race === 'native') {
+            shapeFill = '#2f89cf ' + person.race;
+        }
+        return shapeFill;
+    }
+
+
+    function shapePicker(degree) {
+        if (degree.hasOwnProperty('mdPHD')) {
+            // console.log(degree.mdPHD);
+        }
+    }
+
+    function rankRunner(rank, job) {
+        rank.forEach(function (degree) {
+            if (job.chair) {
+                shape = 'circle';
+            } else if (job.professor) {
+                shape = 'diamond';
+            }
+
+            // checking only MD - filtering
+            filterDegree = 'MD';
+            if (filterDegree) {
+                degreeCheck(degree, filterDegree);
+                if (degreeMatch.length > 0) {
+                    console.log(filterDegree, degreeMatch);
+                }
+            }
+
+            // checking only mdPHD - filtering
+            filterDegree = 'mdPHD';
+            if (filterDegree) {
+                degreeCheck(degree, filterDegree);
+                if (degreeMatch.length > 0) {
+                    console.log(filterDegree, degreeMatch);
+                }
+            }
+
+            // checking only PHD
+            filterDegree = 'PHD';
+            if (filterDegree) {
+                degreeCheck(degree, filterDegree);
+                if (degreeMatch.length > 0) {
+                    console.log(filterDegree, degreeMatch);
+                }
+            }
+
+
+            // filtering females only
+            filterFemale = true;
+            genderCheck(degree);
+            console.log(genderMatch);
+
+            // filtering fills for shape based on degree filter
+
+            // filtering fills for gender filter
+            genderMatch.forEach(function (person) {
+                colorPicker(person);
+                console.log(shapeFill);
+            });
+        })
+    }
+
+    // filter by year
+    function yearSelector (item, year, faculty, degree) {
+        if (year === filterYear) {
+            faculty.forEach(function (job) {
+                if (job.chair) {
+                    rank = job.chair;
+                    rankRunner(rank, job);
+                } else if (job.professor) {
+                    rank = job.professor;
+                    rankRunner(rank.job);
+                }
+            })
+        } else {
+            console.log(year, filterYear)
+        }
+    }
+
+    getData().then(function () {
+        // get list of years
+        var academicYears = [],
+            facultyRank,
+            facultyRace,
+            facultyFemale,
+            facultyMale,
+            facultyTotal;
+
+
+        data.forEach(function (item) {
+            academicYears.push(item.academicYear);
+            year = item.academicYear;
+            faculty = item.faculty;
+            
+            filterYear = 1920;
+            if (filterYear) {
+                yearSelector(item, year, faculty);
+            } else {
+                console.log(item);
+            }
+            
+
+            // filterYear = 1900;
+
+            // yearSelector(year, faculty);
+
+            // get shapes for each position
+            // faculty.forEach(function (job) {
+            //     if (job.chair) {
+            //         rank = job.chair;
+            //         rankRunner(rank, job);
+            //     } else if (job.professor) {
+            //         rank = job.professor;
+            //         rankRunner(rank, job);
+            //     };
+            // })
+        });
+
+    }
+    )
+});
